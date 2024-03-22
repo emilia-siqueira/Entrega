8880cae9bdc65b61ef4cc1b09b8377c530fe22fb
Emília de Siqueira <Emlia de Siqueira@DELLG3-3590>
2024-03-21 21:59:57 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPIDesafio/Controllers/DesafioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Extensions;
+using System.Text.RegularExpressions;
+using WebAPIPessoa.Dominio;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebAPIDesafio.Controllers
+{
+    public class DesafioController : ControllerBase
+    {
+ 
+        [HttpPost("DadosPalindrome")]
+
+        public ActionResult<Palindrome> DadosPalindrome(
+            string textoPalindrome)
+
+        {
+            string reversoTexto = "";
+
+            Palindrome palindrome = new Palindrome();
+
+            for (int i = textoPalindrome.Length-1; i >=0; i--) //Revertendo a string  
+            {
+                reversoTexto += textoPalindrome[i].ToString();
+
+            }
+            if (reversoTexto == textoPalindrome) // Verifica se a String é um palíndromo, ou não.  
+            {
+                palindrome.Texto = "A palavra " + textoPalindrome + " é um Palindromo." ;
+            }
+            else
+            {
+                palindrome.Texto = "A palavra " + textoPalindrome + " não é um Palindromo.";
+            }
+
+            return palindrome;
+
+        }
+
+    }
+}
---
+++ WebAPIDesafio/Dominio/Palindrome.cs
@@ -0,0 +1,17 @@
+﻿namespace WebAPIPessoa.Dominio
+{
+    public class Palindrome
+    {
+        private string texto;
+
+        public string Texto { get => texto; set => texto = value; }
+        public Palindrome(string texto)
+        {
+            this.texto=texto;
+        }
+
+        public Palindrome()
+        {
+        }
+    }
+}
